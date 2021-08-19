ca6a1fe6576882ab90052ce59ac72fd744cc3268
kreynoldsf5 <k.reynolds@f5.com>
2021-08-19 09:17:33 -0400

    more robust date parsing function

--- CleanResources/__init__.py
+++ CleanResources/__init__.py
@@ -3,7 +3,7 @@ import logging
 import os
 
 from volterra_helpers import createVoltSession
-from clean_helpers import getStaleSites
+from clean_helpers import getStaleSites, getStaleUserNSs
 
 import azure.functions as func
 
@@ -31,5 +31,11 @@ def main(cleanTimer: func.TimerRequest) -> None:
         required_vars['VoltTenantApiToken'], required_vars['VoltTenantName'])
     logging.info(s['lastOp'])
 
-    getStaleSites(s, 30)
+    sites = getStaleSites(s, 30)
     logging.info(s['lastOp'])
+
+    ns = getStaleUserNSs(s, 30)
+    logging.info(s['lastOp'])
+
+    logging.info("DEBUG sites: {}".format(sites))
+    logging.info("DEBUG NSs: {}".format(ns))
--- clean_helpers.py
+++ clean_helpers.py
@@ -1,34 +1,55 @@
 import requests
 import logging
 import datetime
+from dateutil.parser import *
 
-from volterra_helpers import updateSO
+from volterra_helpers import updateSO, findUserNS
+
+def findExpiry(staleDays: int):
+    expiry = datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=staleDays)
+    return expiry
 
 def getStaleSites(s: dict, staleDays: int = 30):
-    url = s['urlBase'] + "/api/config/namespaces/system/sites"
+    url = s['urlBase'] + "/api/config/namespaces/system/sites?report_fields"
     try:
         resp = s['session'].get(url)
         resp.raise_for_status()
-        expiry = datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=staleDays)
         staleSites = []
+        expiry = findExpiry(staleDays)
         for item in resp.json()['items']:
+            #Check for FAILED sites last modified before expiry
             if (item['get_spec']['site_type'] == 'CUSTOMER_EDGE') and \
-            (item['get_spec']['site_state'] == 'FAILED') and \
-            (datetime.datetime.fromisoformat(item['system_metadata']['modification_timestamp'].replace('Z', '+00:00')) < expiry):
+                (item['get_spec']['site_state'] == 'FAILED') and \
+                (parse(item['system_metadata']['modification_timestamp']) < expiry):
                 staleSites.append(item['name'])
-        logging.info("DEBUG (stale sites): {0}".format(staleSites))
-        if len(staleSites) > 0:
-            updateSO(s, 'getStaleSites', 'success', 'fetched stale sites')
-            return staleSites
-        else:
-            updateSO(s, 'getStaleSites', 'success', 'No stale sites found')
-            return None
+            #Check for WAITING_FOR_REGISTRATION created before expiry
+            if (item['get_spec']['site_type'] == 'CUSTOMER_EDGE') and \
+                (item['get_spec']['site_state'] == 'WAITING_FOR_REGISTRATION') and \
+                (parse(item['system_metadata']['creation_timestamp']) < expiry):
+                staleSites.append(item['name'])
+        updateSO(s, 'getStaleSites', 'success', 'fetched stale sites')
+        return staleSites
     except requests.exceptions.RequestException as e:
         updateSO(s, 'getStaleSites', 'error', e)
         return None
 
-def cleanStaleSites(s: dict, clusters: list):
-    for cluster in clusters:
+def getStaleUserNSs(s: dict, staleDays: int = 30):
+    url = s['urlBase'] + "/api/web/custom/namespaces/system/user_roles"
+    try:
+        resp = s['session'].get(url)
+        resp.raise_for_status()
+        staleUserNSs = []
+        for item in resp.json()['items']:
+            if (item['last_login_timestamp'] is None) or (parse(item['last_login_timestamp']) < findExpiry(staleDays)):
+                staleUserNSs.append(findUserNS(item['email'])) 
+        updateSO(s, 'getStaleUserNSs', 'success', 'fetched stale user namespaces')
+        return staleUserNSs
+    except requests.exceptions.RequestException as e:
+        updateSO(s, 'getStaleUserNSs', 'error', e)
+        return None
+
+def cleanStaleSites(s: dict, sites: list):
+    for site in sites:
         url = s['urlBase'] + "/api/web/namespaces/system/renew/api_credentials/{}".format(cluster)
         try:
             r = requests.delete(url)
